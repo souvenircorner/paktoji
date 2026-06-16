c48ace7a1cc9fd872219f4fe65a7bb8afe689f2b
souvenircorner <dear.souvenircorner@gmail.com>
2026-06-16 14:21:01 +0700

    Add files via upload

--- src/types/product.ts
+++ src/types/product.ts
@@ -2,7 +2,7 @@ export interface Product {
   id: number
   name: string
   price: number
-  image: string
+  images: string[]
   description: string
   detail: string
   type: 'digital' | 'physical'
